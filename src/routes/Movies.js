2affe6e2d5f49540f8cf99139a60c2b11f18dc7d
pebblepark <subling02@gmail.com>
2022-07-20 00:31:37 +0900

    Feat: Movie 목록 가져오기
    useQuery hook 사용

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,24 @@
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
+
 export default function Movies() {
-  return <div>This is a list of movies.</div>;
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Could not fetch :(</h1>;
+  return (
+    <ul>
+      <h1>Movies</h1>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
