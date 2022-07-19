b572565f80cbb3fbe4343219fb3f92d8fca3c57e
pebblepark <subling02@gmail.com>
2022-07-19 23:08:12 +0900

    Feat: Router 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Movie from './routes/Movie';
+import Movies from './routes/Movies';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Movies />} />
+        <Route path='/movies/:id' element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Movies.js
@@ -0,0 +1,3 @@
+export default function Movies() {
+  return <div>This is a list of movies.</div>;
+}
