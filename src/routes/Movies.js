45b50a796f4e5b345fe5778ba31fa39718c338f6
pebblepark <subling02@gmail.com>
2022-07-20 21:30:32 +0900

    Feat: Movie 상세 페이지 구현
    - useQuery Variables로 id 전달
    - 한번 데이터를 조회해서 가져온 경우 ApolloClient에 설정한 cache 설정으로 인해 refetch 발생 x

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
 const ALL_MOVIES = gql`
   query getMovies {
@@ -17,7 +18,9 @@ export default function Movies() {
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   );
